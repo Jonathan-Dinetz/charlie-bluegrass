f0f66dc59f1f7fa1c38b6dd7afb23bc20140c0ea
Jonathan-Dinetz <58620637+Jonathan-Dinetz@users.noreply.github.com>
2019-12-14 01:28:40 -0800

    Add files via upload

---
+++ Type1.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(100, 100, WEBGL);
+}
+
+function draw() {
+  background(205, 102, 94);
+  sphere(40);
+}
+ 
+$( "div" )
+    .hover(function() {
+      $( this )
+        .toggleClass( "active" )
+       
+         
+    });
+
+
